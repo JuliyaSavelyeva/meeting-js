65c8d5f8e73c0b810c017f535d2497575467b02a
JuliyaSavelyeva <juliyasavelyeva0605@gmail.com>
2021-07-29 11:32:18 +0300

    add

--- react/task3/src/User.jsx
+++ react/task3/src/User.jsx
@@ -1,31 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import { getFetchData } from './gateway.js';
+import { useParams } from 'react-router-dom';
 
-const User = ({ match }) => {
-  const userId = match.params.userId;
-  const [userData, setUserData] = useState({
-    location: '',
-    name: '',
-    avatar_url: '',
-  });
+const User = () => {
+  const [userData, setUserData] = useState(null);
+  const { userId } = useParams();
 
   useEffect(() => {
-    getFetchData(userId).then(({ location, name, avatar_url }) => {
-      setUserData({
-        ...userData,
-        location,
-        name,
-        avatar_url,
+    const baseUrl = 'https://api.github.com/users';
+    fetch(`${baseUrl}/${userId}`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error();
+      })
+      .then(({ avatar_url, name, location }) => {
+        setUserData({
+          userName: name,
+          userLocation: location,
+          userAvatar: avatar_url,
+        });
       });
-    });
   }, [userId]);
+
+  if (!userData) {
+    return null;
+  }
+
+  const { userAvatar, userName, userLocation } = userData;
 
   return (
     <div className="user">
-      <img alt="User Avatar" src={userData.avatar_url} className="user__avatar" />
+      <img alt="User Avatar" src={userAvatar} className="user__avatar" />
       <div className="user__info">
-        <span className="user__name">{userData.name}</span>
-        <span className="user__location">{userData.location}</span>
+        <span className="user__name">{userName}</span>
+        <span className="user__location">{userLocation}</span>
       </div>
     </div>
   );
